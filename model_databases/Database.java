c9fff373ea9fe59649a76e0040c7516d3d22c4df
1849871 <1849871@20.8.20.247>
2020-03-12 11:09:15 -0700

    databases

---
+++ model_databases/Database.java
@@ -0,0 +1,23 @@
+package model_databases;
+
+import java.sql.*;
+
+public class Database {
+	private static Connection con;
+	
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		connect();
+		
+	}
+	
+	public static void connect() throws ClassNotFoundException, SQLException {
+		Class.forName("org.sqlite.JDBC");
+		try {
+			con = DriverManager.getConnection("JDBC:SQLite:Pokemon.db");
+			System.out.println("Connection successful");
+		}
+		catch(Exception e) {
+			System.out.println("Could not get connection " + e);
+		}
+	}
+}
