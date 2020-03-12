848450a43567885410d46e01bce0bcd9cb938ebc
Lucy Gao <real.lucy.gao@gmail.com>
2020-03-12 11:36:32 -0700

    testing purposes

--- src/ui_control/MainMenu.java
+++ src/ui_control/MainMenu.java
@@ -2,10 +2,15 @@ package ui_control;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JButton;
 
 public class MainMenu extends JFrame {
 	public MainMenu() {
 		getContentPane().setLayout(null);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(383, 216, 117, 29);
+		getContentPane().add(btnNewButton);
 		setBounds(100, 100, 800, 600);
 	}
 	
