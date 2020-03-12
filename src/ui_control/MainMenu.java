e4695452a497d3ee82ececcdf8159b74ee1c3db1
Lucy Gao <real.lucy.gao@gmail.com>
2020-03-12 11:28:24 -0700

    added bounds

--- src/ui_control/MainMenu.java
+++ src/ui_control/MainMenu.java
@@ -1,10 +1,24 @@
 package ui_control;
+import java.awt.EventQueue;
+
 import javax.swing.JFrame;
 
 public class MainMenu extends JFrame {
+	public MainMenu() {
+		getContentPane().setLayout(null);
+		setBounds(100, 100, 800, 600);
+	}
 	
-	public static void main (String args[])
-	{
-		System.out.println("bruh");
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainMenu frame = new MainMenu();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			} 
+		});
 	}
 }
