c055a2f8061b9890c407eb816a6989ee3865cb9e
1901209 <1901209@20.8.21.1>
2020-03-12 11:36:14 -0700

    MVC

---
+++ src/model/Database_Methods.java
@@ -0,0 +1,24 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database_Methods {
+	public static Connection connect(String input) {
+		//Url format is like: jdbc:sqlite:src/Model/amazon2.db
+		
+		String url = input;
+		
+		Connection conn = null;
+		
+		try {
+			conn = DriverManager.getConnection(url);
+		}
+		catch(SQLException e) {
+			System.out.print(e.getMessage());
+		}
+		
+		return conn;
+	}
+}
