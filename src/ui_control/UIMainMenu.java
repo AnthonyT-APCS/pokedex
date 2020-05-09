fc4abbb5043a182d1aa10fdc3c99fb5a99a9958d
lucyygao <42822456+lucyygao@users.noreply.github.com>
2020-05-08 20:21:09 -0700

    adding button pic

--- src/ui_control/UIMainMenu.java
+++ src/ui_control/UIMainMenu.java
@@ -7,6 +7,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
+import javax.swing.SwingConstants;
 import javax.swing.border.Border;
 
 import model.CircleQueueDriver;
@@ -26,6 +27,7 @@ public class UIMainMenu extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1162, 806);
         java.awt.Image image = new ImageIcon("img/background.PNG").getImage();
+        java.awt.Image image2 = new ImageIcon("img/button.JPG").getImage();
         
         // Content Panel to add Label and Image
         //https://docs.oracle.com/javase/tutorial/uiswing/components/rootpane.html
@@ -54,13 +56,27 @@ public class UIMainMenu extends JFrame {
         text.setBounds(180, 663, 159, 61);
         getContentPane().add(text);
         
-       /* // additional power button
+        // power button
         JButton power = new JButton("");
-        power.setEnabled(false);
-        power.setBackground(new Color(30, 144, 255));
-        power.setFont(new Font("Tahoma", Font.PLAIN, 11));
-        power.setBounds(44, 69, 73, 68);
-        getContentPane().add(power);*/
+        //power.setEnabled(false);
+        //power.setForeground(new Color(255, 255, 255));
+        power.setIcon(new ImageIcon(image2));
+        power.setBounds(27, 48, 119, 115);
+        getContentPane().add(power);
+        power.setVisible(false);
+        
+        JLabel lblNewLabel = new JLabel("APCS: POKEDEX PROJECT");
+        lblNewLabel.setFont(new Font("Yu Gothic UI Semibold", Font.BOLD, 20));
+        lblNewLabel.setBounds(790, 31, 307, 45);
+        getContentPane().add(lblNewLabel);
+        
+        JLabel lblNewLabel_1 = new JLabel("APCS: POKEDEX PROJECT");
+        lblNewLabel_1.setFont(new Font("Yu Gothic UI Semibold", Font.BOLD, 20));
+        lblNewLabel_1.setBounds(790, 74, 307, 45);
+        getContentPane().add(lblNewLabel_1);
+        //power.setHorizontalTextPosition(SwingConstants.CENTER);
+        
+        	
         
         // JLabel with image  
         JLabel pic = new JLabel("");
@@ -80,8 +96,8 @@ public class UIMainMenu extends JFrame {
         			pokemons = pokemons + "\n" + pokemonl.get(x);
         		}
         		display.setText(pokemons);
-        		power.setBackground(new Color(0, 255, 255));
-        		
+        		power.setVisible(true);
+        		//power.setIcon(new ImageIcon(image2));
         	}
         });
 	}
