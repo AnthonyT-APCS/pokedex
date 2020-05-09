ea224a8fe02eb92d392490aee61a15891909d20b
lucyygao <42822456+lucyygao@users.noreply.github.com>
2020-05-08 17:39:11 -0700

    added background picture

--- src/ui_control/UIMainMenu.java
+++ src/ui_control/UIMainMenu.java
@@ -5,14 +5,24 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.border.Border;
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
+import java.awt.Container;
 
 public class UIMainMenu extends JFrame {
 	public UIMainMenu() {
-		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1162, 806);
+        java.awt.Image image = new ImageIcon("background.PNG").getImage();
+        
+        // Content Panel to add Label and Image
+        //https://docs.oracle.com/javase/tutorial/uiswing/components/rootpane.html
+        Container content = getContentPane();
+        content.setBackground(Color.LIGHT_GRAY);
+        getContentPane().setLayout(null);
 	}
 
 	public static void main(String[] args) 
